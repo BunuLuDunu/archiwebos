71ede487faa5f6ae8ebab84eeccb7ab358c9f5fb
BunuLuDunu <laurine.sassano@gmail.com>
2023-03-28 09:45:00 +0200

    Created filter buttons

--- FrontEnd/assets/works.js
+++ FrontEnd/assets/works.js
@@ -2,19 +2,74 @@
 const response = await fetch('http://localhost:5678/api/works');
 const works = await response.json();
 
-works.forEach(work => {
-    //Récupération de l'élément du DOM qui accueillera les projets
-    const gallery = document.querySelector(".gallery");
-    //Création d'une balise dédiée à un projet
-    const workElement = document.createElement("figure");
-    const imageElement = document.createElement("img");
-    imageElement.src = work.imageUrl;
-    const titleElement =  document.createElement("figcaption");
-    titleElement.innerText = work.title;
+//Récupération de l'élément du DOM qui accueillera les projets
+const gallery = document.querySelector(".gallery");
 
-    //Rattachement de la balise figure à la div gallery
-    gallery.appendChild(workElement);
-    //Rattachement de l'image à la balise figure
-    workElement.appendChild(imageElement);
-    workElement.appendChild(titleElement);
-})
+function createGallery(works) {
+    gallery.innerHTML = "";
+    works.forEach(work => {
+        //Création de chaque projet en HTML
+        const workElement = document.createElement("figure");
+        const imageElement = document.createElement("img");
+        imageElement.setAttribute("alt", work.title);
+        imageElement.src = work.imageUrl;
+        const titleElement =  document.createElement("figcaption");
+        titleElement.innerText = work.title;
+
+        //Rattachement de la balise figure à la div gallery
+        gallery.appendChild(workElement);
+        //Rattachement de l'image à la balise figure
+        workElement.appendChild(imageElement);
+        workElement.appendChild(titleElement);
+    })
+};
+
+createGallery(works);
+
+//Récupération des catégories pour chaque projet
+let workCategories = works.map(work => {
+    return work.category.name;
+    
+});
+
+//Création d'un set avec les 3 catégories
+workCategories = new Set(workCategories);
+
+//Récupération de l'élément du DOM qui accueillera les boutons
+const portfolio = document.querySelector("#portfolio");
+//Création d'une div pour accueillir les boutons
+const galleryMenu = document.createElement("div");
+galleryMenu.classList.add("gallery-menu");
+//Rattachement de la div gallery-menu à la section portfolio
+portfolio.insertBefore(galleryMenu, gallery);
+
+function createCategoryButton(category, checked) {
+    //Création des boutons
+    const galleryCategory = document.createElement("input")
+    galleryCategory.type = "radio";
+    galleryCategory.id = category;
+    galleryCategory.name = "category";
+    galleryCategory.value = category;
+    galleryCategory.checked = checked;
+    //Création de chaque label
+    const categoryLabel = document.createElement("label");
+    categoryLabel.setAttribute("for", category);
+    categoryLabel.innerText = category;
+    //Ajout de l'écouteur d'évènement pour filtrer au clic
+    galleryCategory.addEventListener("click", e => {
+       const filteredWorks = works.filter(function (work) {
+        return work.category.name == galleryCategory.value || galleryCategory.value == "Tous";
+       });
+       createGallery(filteredWorks);
+    });
+
+    //Rattachement des boutons à la div gallery-menu
+    galleryMenu.appendChild(galleryCategory);
+    galleryMenu.appendChild(categoryLabel);
+    
+}
+//Création des boutons radio pour chaque catégorie
+createCategoryButton("Tous", true);
+workCategories.forEach(category => {
+    createCategoryButton(category, false)
+});
